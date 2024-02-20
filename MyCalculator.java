96ef6fffccf8d7a04073838e2a17320c23f283ac
Sahand Omarzai <sahand.omarzai@gmail.com>
2024-02-20 18:58:43 +0100

    initial program

---
+++ MyCalculator.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MyCalculator{
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame("myCalc");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setVisible(true);
+
+        
+    }
+}
